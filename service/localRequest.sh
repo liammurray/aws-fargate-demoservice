50d97da601f9f1f08eb3d0bbf96e20748484ae70
Liam Murray <liammurray@users.noreply.github.com>
2020-03-22 15:07:06 -0700

    Tweaks for docker

--- service/localRequest.sh
+++ service/localRequest.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+PORT=3005
 if [ $# != 2 ]; then
   >&2 echo "Usage: ./localRequest.sh <method> <endpoint>"
   exit 1
 fi
-curl -sk -w "%{http_code}" -X $1 localhost:3000/v1/$2
+curl -sk -w "%{http_code}" -X $1 localhost:3005/v1/$2
 
